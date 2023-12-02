2ef68b0eaeabd715316705a2952eb3e87f3e1829
yasithranusha <yasithranusha24@gmail.com>
2023-12-02 13:02:13 +0530

    feat(exception): add Todo API Exception

---
+++ src/main/java/com/yasith/todo/exception/TodoAPIException.java
@@ -0,0 +1,12 @@
+package com.yasith.todo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public class TodoAPIException extends RuntimeException{
+    private HttpStatus httpStatus;
+    private String message;
+}
